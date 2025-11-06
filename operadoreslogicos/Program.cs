f709e64863828db97d22ee6ffdc702585b18d83d
ISABELA GOMES NASCIMENTO <isabela.gnascimento4@senacsp.edu.br>
2025-11-06 14:57:56 -0300

    Enviando atualização

---
+++ operadoreslogicos/Program.cs
@@ -0,0 +1,13 @@
+﻿// Operadores lógicos
+bool x = true;
+bool y = false;
+
+bool and = x && y; // E
+bool or = x || y; // OU
+bool not = !x; // NÃO
+
+// Exibindo resultados dos operadores lógicos
+Console.WriteLine("\nOperadores Lógicos");
+Console.WriteLine($"x && y: {and}");
+Console.WriteLine($"x || y : {or}");
+Console.WriteLine($"!x: {not}");
